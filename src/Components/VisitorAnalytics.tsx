528f6ec9c7fbad5ea1f1daf773e23f04451b72b5
Ritwik880 <rittiksinha12@gmail.com>
2022-09-18 11:32:16 +0530

    fix

--- src/Components/VisitorAnalytics.tsx
+++ src/Components/VisitorAnalytics.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Cta from './Cta'
 
 const Visitor = () => {
+    const [count, setCount] = useState(1400000);
+
+    window.onload = (event) => {
+        setCount(count + 500);
+    };
+
+
+
     return (
         <>
             <div className="pb-6 d-flex align-items-center refund-page">
@@ -15,10 +23,11 @@ const Visitor = () => {
             <section className='refundSection'>
                 <div className="row container">
                     <h1 className='refundHead'>Visitor Analytics</h1>
-                    <p className='aboutPara'>To process the Applications Food Processing Corporation of India (FPCI) charges Nominal Application Fee/Intimation Charges for all its openings. Application Fee/Intimation Charges or any type of Application Fee is Non-Refundable and payment of fee does not guarantee selection of candidate for aforesaid job.Application Fee/Intimation Charges is being charged to ensure the smooth process of Interview of ‘Interested/short-listed candidates only’. FPCI gives chance to all short-listed eligible candidates to appear in the screening/interview and for the same, we schedule interviews for all selected/short-listed candidates. Any cancellation or refund request will not be considered.</p>
-
-
+                    <h4 className='count'>
+                        {count}
+                    </h4>
                 </div>
+
             </section>
             <Cta />
         </>
